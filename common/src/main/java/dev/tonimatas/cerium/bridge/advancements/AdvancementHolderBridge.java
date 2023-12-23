2b6fe38c8601f443062bba50c18c62d5bf48a957
TonimatasDEV <amatascubero@gmail.com>
2023-12-23 03:14:35 +0100

    Advancement patches

---
+++ common/src/main/java/dev/tonimatas/cerium/bridge/advancements/AdvancementHolderBridge.java
@@ -0,0 +1,5 @@
+package dev.tonimatas.cerium.bridge.advancements;
+
+public interface AdvancementHolderBridge {
+    org.bukkit.advancement.Advancement bridge$toBukkit();
+}
---
+++ common/src/main/java/dev/tonimatas/cerium/mixins/advancements/AdvancementHolderMixin.java
@@ -0,0 +1,14 @@
+package dev.tonimatas.cerium.mixins.advancements;
+
+import dev.tonimatas.cerium.bridge.advancements.AdvancementHolderBridge;
+import net.minecraft.advancements.AdvancementHolder;
+import org.bukkit.craftbukkit.v1_20_R3.advancement.CraftAdvancement;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AdvancementHolder.class)
+public class AdvancementHolderMixin implements AdvancementHolderBridge {
+    @Override
+    public final org.bukkit.advancement.Advancement bridge$toBukkit() {
+        return new CraftAdvancement((AdvancementHolder) (Object) this);
+    }
+}
